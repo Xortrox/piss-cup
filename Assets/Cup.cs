2028878225d7c9e80f6f07af503c9851b5c14dc9
Anders Morgan Larsen <andy@furious.no>
2020-09-09 22:47:46 +0200

    Add project files

---
+++ Assets/Cup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cup : MonoBehaviour
+{
+    public Animator Animator;
+
+    public int TimeHash;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+
+        TimeHash = Animator.StringToHash("Time");
+    }
+
+    void SetLevel(float level)
+    {
+        Animator.SetFloat(TimeHash, level);
+    }
+}
